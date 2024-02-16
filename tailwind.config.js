0d00cb2171052be659a487139c129f310dbdc3b6
Hatem Hosny <hatemhosny@gmail.com>
2024-02-16 23:14:15 +0200

    scope tailwind base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import { scopedPreflightStyles } from "tailwindcss-scoped-preflight";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    scopedPreflightStyles({
+      cssSelector: "#banner",
+      mode: "matched only",
+    }),
+  ],
 };
